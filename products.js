8b702b2c3c48a80dca743823f80ef1635880923e
FouadMohamed <76663024+fouadmo1223@users.noreply.github.com>
2023-08-21 00:44:05 +0300

    Add files via upload

---
+++ products.js
@@ -0,0 +1,69 @@
+var products = [
+    {
+      id: 1,
+      title: "product one",
+      description:"product One is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged",
+      price: 700,
+      discountPercentage: 12.96,
+     
+      stock: 10,
+     
+      img: "https://img.freepik.com/free-photo/brush-stroke-banner-background_1361-3550.jpg?t=st=1661055012~exp=1661055612~hmac=7a599d103bd9b160223bb71ae2f362cf1b81f6d3867300355511d02d7cb204cf",
+    },
+    {
+      id: 2,
+      title: "product two",
+      description:"product One is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged",
+      price: 1500,
+      discountPercentage: 17.94,
+     
+      stock: 20,
+    
+      img: "https://img.freepik.com/free-photo/modern-brush-stroke-banner_1361-3526.jpg?t=st=1661055012~exp=1661055612~hmac=a5fca1f34d92d17ed5be4fe60cc28e5f870b768ca6b51518a58780082ab8f859",
+    },
+    {
+      id: 3,
+      title: "product three",
+      description:"product One is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged",
+      price: 200,
+      discountPercentage: 15.46,
+      
+      stock: 8,
+     
+      img: "https://img.freepik.com/free-photo/modern-frame-with-golden-splash_1361-3529.jpg?t=st=1661055012~exp=1661055612~hmac=ae6de6e547330bc5c4be7580ce06b2937420802d2db6a75d0aee104905f7e666",
+    },
+    {
+      id: 4,
+      title: "product four",
+      description:"product One is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged",
+      price: 850,
+      discountPercentage: 17.91,
+      
+      stock: 7,
+   
+      img: "https://img.freepik.com/free-photo/red-black-brush-stroke-banner-background-perfect-canva_1361-3597.jpg?t=st=1661055012~exp=1661055612~hmac=22d7609e42ecff68c25e66a8fdfaa4b158951452a9998dbb7baa4e390c60ce3e",
+    },
+    {
+      id: 5,
+      title: "product five",
+      description:"product One is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged",
+      price: 499,
+      discountPercentage: 10.58,
+   
+      stock: 15,
+      
+      img: "https://img.freepik.com/free-photo/modern-brush-stroke-banner_1361-3526.jpg?t=st=1661055012~exp=1661055612~hmac=a5fca1f34d92d17ed5be4fe60cc28e5f870b768ca6b51518a58780082ab8f859",
+    },
+    {
+      id: 6,
+      title: "product six",
+      description:"product One is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged",
+      price: 1749,
+      discountPercentage: 11.02,
+    
+      stock: 7,
+     
+      
+      img: "https://img.freepik.com/free-photo/modern-brush-stroke-banner_1361-3526.jpg?t=st=1661055012~exp=1661055612~hmac=a5fca1f34d92d17ed5be4fe60cc28e5f870b768ca6b51518a58780082ab8f859",
+    },
+  ];
